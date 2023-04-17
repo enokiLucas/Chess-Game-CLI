88e66f739be394567e26619c2d8bdc27bd9e1922
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-17 15:16:32 -0300

    Create valid moves

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -10,6 +10,25 @@ class Bishop < Piece
   end
 
   def valid_moves
-    # return an array of valid moves for the bishop
+    valid_moves = []
+    current_row, current_col = @pos
+
+    # check moves in diagonal directions
+    (-7..7).each do |i|
+      if i != 0
+        new_row = current_row + i
+        new_col = current_col + i
+        if new_row.between?(0, 7) && new_col.between?(0, 7)
+          valid_moves << [new_row, new_col]
+        end
+        new_row = current_row + i
+        new_col = current_col - i
+        if new_row.between?(0, 7) && new_col.between?(0, 7)
+          valid_moves << [new_row, new_col]
+        end
+      end
+    end
+
+    valid_moves
   end
 end
