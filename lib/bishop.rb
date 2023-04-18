f9a4a39395130c70c394e4af707ac390993596a8
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-18 16:49:03 -0300

    bishop no longer jumps over other pieces

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -10,25 +10,35 @@ class Bishop < Piece
   end
 
   def valid_moves
-    valid_moves = []
-    current_row, current_col = @pos
-
-    # check moves in diagonal directions
-    (-7..7).each do |i|
-      if i != 0
-        new_row = current_row + i
-        new_col = current_col + i
-        if new_row.between?(0, 7) && new_col.between?(0, 7)
-          valid_moves << [new_row, new_col]
-        end
-        new_row = current_row + i
-        new_col = current_col - i
-        if new_row.between?(0, 7) && new_col.between?(0, 7)
-          valid_moves << [new_row, new_col]
-        end
-      end
-    end
-
-    valid_moves
-  end
+		valid_moves = []
+		current_row, current_col = @pos
+	
+		# check moves in diagonal directions
+		[-1, 1].each do |row_dir|
+			[-1, 1].each do |col_dir|
+				(1..7).each do |distance|
+					new_row = current_row + row_dir * distance
+					new_col = current_col + col_dir * distance
+					if new_row.between?(0, 7) && new_col.between?(0, 7)
+						if @board[new_row, new_col].nil?
+							# If the square is empty, add it to the list of valid moves
+							valid_moves << [new_row, new_col]
+						elsif @board[new_row, new_col].color != @color
+							# If the square is occupied by an opponent's piece, add it to the list of valid moves and stop checking in this direction
+							valid_moves << [new_row, new_col]
+							break
+						else
+							# If the square is occupied by one of the player's own pieces, stop checking in this direction
+							break
+						end
+					else
+						# If we've reached the edge of the board, stop checking in this direction
+						break
+					end
+				end
+			end
+		end
+	
+		valid_moves
+	end
 end
