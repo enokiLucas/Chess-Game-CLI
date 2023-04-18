15943d3b5aa55a832b8cf67115da385578d82e92
Lucas Enoki <enoki.lucas@gmail.com>
2023-04-18 17:11:19 -0300

    Fix #valid_moves

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -9,36 +9,41 @@ class Bishop < Piece
     @color == :white ? "♗".colorize(:blue) : "♝".colorize(:red)
   end
 
-  def valid_moves
-		valid_moves = []
-		current_row, current_col = @pos
-	
-		# check moves in diagonal directions
-		[-1, 1].each do |row_dir|
-			[-1, 1].each do |col_dir|
-				(1..7).each do |distance|
-					new_row = current_row + row_dir * distance
-					new_col = current_col + col_dir * distance
-					if new_row.between?(0, 7) && new_col.between?(0, 7)
-						if @board[new_row, new_col].nil?
-							# If the square is empty, add it to the list of valid moves
-							valid_moves << [new_row, new_col]
-						elsif @board[new_row, new_col].color != @color
-							# If the square is occupied by an opponent's piece, add it to the list of valid moves and stop checking in this direction
-							valid_moves << [new_row, new_col]
-							break
-						else
-							# If the square is occupied by one of the player's own pieces, stop checking in this direction
-							break
-						end
-					else
-						# If we've reached the edge of the board, stop checking in this direction
-						break
-					end
-				end
-			end
-		end
-	
-		valid_moves
-	end
+  def valid_moves(end_pos)
+    valid_moves = []
+    current_row, current_col = @pos
+    end_row, end_col = end_pos
+
+    # check if end position is on the same diagonal as the bishop
+    return valid_moves unless (current_row - end_row).abs == (current_col - end_col).abs
+
+    # determine the direction of the diagonal movement
+    row_dir = end_row <=> current_row
+    col_dir = end_col <=> current_col
+
+    # check each position along the diagonal path
+    row, col = current_row + row_dir, current_col + col_dir
+    while row != end_row && col != end_col
+      return valid_moves unless row.between?(0, 7) && col.between?(0, 7)
+
+      if @board[row, col].nil?
+        valid_moves << [row, col]
+      else
+        break
+      end
+
+      row += row_dir
+      col += col_dir
+    end
+
+    # check if the end position is valid
+    if row.between?(0, 7) && col.between?(0, 7) && (@board[row, col].nil? || @board[row, col].color != @color)
+      valid_moves << [row, col]
+    end
+
+    valid_moves
+  end
+
+
+
 end
